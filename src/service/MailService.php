a06b76b766672dcbb1cf5f88f94c02ee5b8a3482
KvRae <karamelmannai@gmail.com>
2022-07-30 20:17:41 +0100

    Majour Update

---
+++ src/service/MailService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\service;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MailService
+{
+    private MailerInterface $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+    }
+
+    public function sendEmail(string $to): void
+    {
+        $email = (new Email())
+            ->from('kvr4business@gmail.com')
+            ->to('you@example.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $this->mailer->send($email);
+
+        // ...
+    }
+}
