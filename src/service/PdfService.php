38a33eccc1e657dcc5ab20865647c2be3b3453d0
KvRae <karamelmannai@gmail.com>
2022-07-31 22:52:55 +0100

    fixing template issue #1

--- src/service/PdfService.php
+++ src/service/PdfService.php
@@ -15,13 +15,22 @@ class PdfService
         $this->domPdf = new Dompdf();
         $this->domPdf->setPaper('A4', 'portrait');
         $pdfOptions->isRemoteEnabled(true);
+        $dompdf = new Dompdf($pdfOptions);
+        $contxt = stream_context_create([
+            'ssl' => [
+                'verify_peer' => FALSE,
+                'verify_peer_name' => FALSE,
+                'allow_self_signed' => TRUE
+            ]
+        ]);
+        $dompdf->setHttpContext($contxt);
+
         //$pdfOptions->setIsHtml5ParserEnabled(true);
         $this->domPdf->setOptions($pdfOptions);
 
 
 
     }
-
     public function showPdfFile($html){
         $this->domPdf->loadHtml($html);
         $this->domPdf->render();
@@ -31,7 +40,6 @@ class PdfService
         ]);
 
     }
-
     public function generateBinaryPDF($html){
         $this->domPdf->loadHtml($html);
         $this->domPdf->render();
